c123b905efe64cc4c0a30fa1c687f9474ff9485e
Mohamed Algebali Almoazin <48735987+Mohamed01555@users.noreply.github.com>
2023-12-30 20:31:41 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,36 +10,36 @@ def decode_unicode(text):
 # Define your FAQ questions and answers
 def FAQs():
     faq = {
-        "Q: What is VitaLink Pro?":"A: VitaLink Pro is a personalized virtual health assistant (PVHA) designed to help you achieve your health and fitness goals. It provides tailored advice, workout plans, and nutrition guidance based on your individual profile.",
-        "Q: What key capabilities does VitaLink Pro offer?<ul>" :
+        "What is VitaLink Pro?":"A: VitaLink Pro is a personalized virtual health assistant (PVHA) designed to help you achieve your health and fitness goals. It provides tailored advice, workout plans, and nutrition guidance based on your individual profile.",
+        "What key capabilities does VitaLink Pro offer?<ul>" :
         " <li>**Personalized Health Plans: **: Tailored workout routines and nutrition guidance based on your unique profile.</li>"
         " <li>**Adaptive Algorithms: **Advanced algorithms that adjust recommendations over time to match your progress.</li>"
         " <li>**Dietary Guidance:**: Customized nutritional advice to support your health and fitness goals.</li>"
         " <li>**User Privacy:**: Strict adherence to privacy and data protection regulations to ensure the security of your personal information.</li>" ,
     
-        "Q: How can I benefit from using VitaLink Pro?":
+        "How can I benefit from using VitaLink Pro?":
         "A: VitaLink Pro offers personalized health plans, exercise routines, and nutrition advice to help you reach your fitness goals. It adapts to your current fitness level and preferences, making it a valuable tool for anyone seeking a healthier lifestyle.",
         
         "What makes VitaLink Pro different from other health apps?":
         "A: VitaLink Pro stands out with its advanced language models and personalized approach. It takes into account your unique health profile, providing tailored advice and recommendations for a more effective fitness journey.",
-        "Q: How does VitaLink Pro generate personalized health plans?":"    A: VitaLink Pro uses advanced algorithms and user-provided information, such as fitness level, health goals, and preferences, to generate personalized health plans. It adapts over time, ensuring the recommendations stay relevant to your progress.",
+        "How does VitaLink Pro generate personalized health plans?":"    A: VitaLink Pro uses advanced algorithms and user-provided information, such as fitness level, health goals, and preferences, to generate personalized health plans. It adapts over time, ensuring the recommendations stay relevant to your progress.",
         
-        "Q: Can VitaLink Pro help with specific health conditions or fitness goals?" :
+        "Can VitaLink Pro help with specific health conditions or fitness goals?" :
         "A: Yes, VitaLink Pro is designed to assist users with a variety of health conditions and fitness goals. Whether you're looking to lose weight, build muscle, or manage specific health concerns, the app tailors its recommendations to meet your needs.",
         
-        "Q: Is my personal information safe and secure on VitaLink Pro?":
+        "Is my personal information safe and secure on VitaLink Pro?":
         "A: Yes, we prioritize user privacy and data security. Your personal information is kept confidential, and we adhere to strict privacy and data protection regulations to ensure a secure user experience." ,
 
-        "Q: Can I use VitaLink Pro if I'm a beginner in fitness?" : 
+        "Can I use VitaLink Pro if I'm a beginner in fitness?" : 
         "A: Absolutely! VitaLink Pro is designed for users of all fitness levels, from beginners to advanced. The app adapts to your current fitness level and provides guidance suitable for your experience and goals.",
         
-        "Q: How often should I use VitaLink Pro for optimal results?":
+        "How often should I use VitaLink Pro for optimal results?":
         "A: For optimal results, aim to incorporate VitaLink Pro into your daily routine. Follow the personalized plans, engage in regular physical activity, and make healthy lifestyle choices. Consistency is key to seeing positive changes.",
         
-        "Q: Can I use VitaLink Pro for dietary guidance?":
+        "Can I use VitaLink Pro for dietary guidance?":
         "A: Yes, VitaLink Pro includes dietary guidance as part of its personalized approach. It provides recommendations on nutrition based on your goals and preferences, helping you make informed choices for a balanced diet.",
 
-        "Q: How does VitaLink Pro ensure user engagement and motivation?":
+        "How does VitaLink Pro ensure user engagement and motivation?":
         "A: VitaLink Pro fosters engagement through personalized plans, progress tracking, and regular updates. The app's adaptive nature and variety in recommendations keep users motivated on their fitness journey.",
 
         "What is the chunk size slider for?":
